0de6151087fa1fc7b347b66d2688b34a1aba26d6
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-04-13 14:58:55 +0100

    env setup

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import '@oasisprotocol/sapphire-hardhat';
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.17",
+        networks: {
+            'sapphire-testnet': {
+                url: "https://testnet.sapphire.oasis.io",
+                accounts: process.env.PRIVATE_KEY
+                    ? [process.env.PRIVATE_KEY]
+                    : [],
+                chainId: 0x5aff,
+            },
+        },
+    };
+
+ export default config;
